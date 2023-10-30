0d961282f02b610ca19c8d5594b5c6b70457a044
Michał Kaczmarek <michalkaczmarek788@gmail.com>
2023-10-30 15:29:49 +0100

    Add documentation to function

--- palindrome.py
+++ palindrome.py
@@ -2,9 +2,16 @@ example_text1 = "potop"
 example_text2 = "test"
 
 def checkIsPalindrome(text):
+    """
+       Checks whether the given argument is a palindrome.
+
+        text
+            passed string value to check
+        True or False value is returned
+    """
     reverseTxt = text[::-1]
     if reverseTxt == text:
         return True
     return False
 
-checkIsPalindrome(example_text2)
+checkIsPalindrome(example_text1)
